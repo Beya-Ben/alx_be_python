c1fbd69f5ae7df908add14965597c5acf1459355
Beya <beya.ben2308@gmail.com>
2024-06-30 12:14:34 +0800

    task4

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,9 +1,8 @@
-# Define the variables
-length = 10  # Length of the rectangle
-width = 5    # Width of the rectangle
+# Define the variable
+hours = 2  # Number of hours to convert
 
-# Calculate the area of the rectangle
-area = length * width
+# Calculate the number of seconds in the given hours
+seconds = hours * 3600  # 3600 seconds in an hour
 
-# Print the calculated area
-print(f"The area of the rectangle is: {area}")
+# Print the result
+print(f"{hours} hour(s) is {seconds} seconds.")
