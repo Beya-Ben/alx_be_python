109dd314233b43c96250e920f11ef9f40e90190f
Beya <beya.ben2308@gmail.com>
2024-07-07 14:00:31 +0800

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
 # temp_conversion_tool.py
 
 # Define global conversion factors using variables
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR\s*=\s*5\/9
+CELSIUS_TO_FAHRENHEIT_FACTOR\s*=\s*9\/5
